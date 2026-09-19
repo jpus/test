25bbf69a9b5f49a38de759026bd595ae37490f28
jpus <zengqcyxx@gmail.com>
2026-09-20 03:05:11 +0800

    Add files via upload

---
+++ komari-agent.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+export AGENT_ENDPOINT=${AGENT_ENDPOINT:-}
+export AGENT_TOKEN=${AGENT_TOKEN:-}
+
+set_download_url() {
+  local default_url="$1"
+  local x64_url="$2"
+
+  case "$(uname -m)" in
+    x86_64|amd64|x64) echo "$x64_url" ;;
+    *) echo "$default_url" ;;
+  esac
+}
+
+download_program() {
+  local program_name="$1"
+  local default_url="$2"
+  local x64_url="$3"
+
+  local download_url
+  download_url=$(set_download_url "$default_url" "$x64_url")
+
+  if [ ! -f "$program_name" ]; then
+    echo "Downloading $program_name..."
+    curl -sSL "$download_url" -o "$program_name"
+    chmod +x "$program_name"
+    echo "$program_name Download completed and permission granted."
+  else
+    echo "$program_name Already exists, skip downloading."
+  fi
+}
+
+download_program "komari" "https://github.com/luodaoyi/komari-zig-agent/releases/download/v0.1.49/komari-agent-linux-arm64" "https://github.com/luodaoyi/komari-zig-agent/releases/download/v0.1.49/komari-agent-linux-amd64"
+sleep 6
+
+run() {
+  if [ -x komari ]; then
+    if ! pgrep -f "./komari" >/dev/null; then
+      nohup ./komari >/dev/null 2>&1 &
+      echo "komari Service started"
+    else
+      echo "komari Service already running"
+    fi
+  fi
+}
+run
+
+while true; do
+  if [ -x komari ]; then
+    if ! pgrep -f "./komari" >/dev/null; then
+      nohup ./komari >/dev/null 2>&1 &
+      echo "komari The service has been restarted"
+    fi
+  fi
+  
+  sleep 120
+done
